908b6a634391ff4d75c2c675cc74265496d7c30f
igorgoremykin <c-sharp@bk.ru>
2021-11-11 22:38:50 +0200

    Format LoginView.

--- src/preact/views/LoginView.jsx
+++ src/preact/views/LoginView.jsx
@@ -1,40 +1,40 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import classNames from 'classnames';
 
-import {Logo} from '../components/Logo'
-import {Link} from "react-router-dom";
+import {Logo} from '../components/Logo';
 import usePresenter from '../usePresenter';
 
 export default function LoginView(props) {
-  const { controller, model } = usePresenter(props);
+  const {controller, model} = usePresenter(props);
 
-    console.log('[LoginView # render] model:', model, ' count:', this.state.count);
+  console.log('[LoginView # render] model:', model);
 
-    const emailInputClass = classNames('input input-bordered', {
-      'input-primary': model.isEmailInputStatePrimary,
-      'input-error': model.isEmailInputStateError,
-    });
-    const passwordInputClass = classNames('input input-bordered', {
-      'input-primary': model.isPasswordInputStatePrimary,
-      'input-error': model.isPasswordInputStateError,
-    });
-    const createAccountLinkClass = classNames(
+  const emailInputClass = classNames('input input-bordered', {
+    'input-primary': model.isEmailInputStatePrimary,
+    'input-error': model.isEmailInputStateError,
+  });
+  const passwordInputClass = classNames('input input-bordered', {
+    'input-primary': model.isPasswordInputStatePrimary,
+    'input-error': model.isPasswordInputStateError,
+  });
+  const createAccountLinkClass = classNames(
       'link link-neutral link-hover select-none',
       {
         'pointer-events-none': model.createAccountLinkDisabled,
-        'text-base-300': model.createAccountLinkDisabled
-      }
-    );
-    const signInButtonClass = classNames('btn btn-primary', {
-      'loading': model.signInButtonLoading
-    });
+        'text-base-300': model.createAccountLinkDisabled,
+      },
+  );
+  const signInButtonClass = classNames('btn btn-primary', {
+    'loading': model.signInButtonLoading,
+  });
 
-    return (
+  return (
       <div className="container mx-auto p-4 space-y-6">
         <div className="flex items-center flex-col">
           <div className="w-80 space-y-6">
             <div
-              className="card shadow-md lg:card-side bg-primary text-primary-content"
+                className="card shadow-md lg:card-side bg-primary text-primary-content"
             >
               <div className="card-body p-3 h-24">
                 <Logo className="object-center"/>
@@ -46,12 +46,12 @@ export default function LoginView(props) {
                   <span className="label-text">Email</span>
                 </label>
                 <input
-                  placeholder="Please enter your email"
-                  className={emailInputClass}
-                  type="text"
-                  disabled={model.emailInputDisabled}
-                  onChange={controller.onEmailChange}
-                  onBlur={controller.onEmailFocusOut}
+                    placeholder="Please enter your email"
+                    className={emailInputClass}
+                    type="text"
+                    disabled={model.emailInputDisabled}
+                    onChange={controller.onEmailChange}
+                    onBlur={controller.onEmailFocusOut}
                 />
                 <label className="label">
                   <span className="label-text-alt">
@@ -64,12 +64,12 @@ export default function LoginView(props) {
                   <span className="label-text">Password</span>
                 </label>
                 <input
-                  placeholder="Please enter your password"
-                  className={passwordInputClass}
-                  type="password"
-                  disabled={model.passwordInputDisabled}
-                  onChange={controller.onPasswordChange}
-                  onBlur={controller.onPasswordFocusOut}
+                    placeholder="Please enter your password"
+                    className={passwordInputClass}
+                    type="password"
+                    disabled={model.passwordInputDisabled}
+                    onChange={controller.onPasswordChange}
+                    onBlur={controller.onPasswordFocusOut}
                 />
                 <label className="label">
                   <span className="label-text-alt">
@@ -80,8 +80,8 @@ export default function LoginView(props) {
             </div>
             <div className="form-control">
               <button
-                className={signInButtonClass}
-                onClick={controller.onSignInClick}
+                  className={signInButtonClass}
+                  onClick={controller.onSignInClick}
               >
                 Sign In
               </button>
@@ -94,5 +94,5 @@ export default function LoginView(props) {
           </div>
         </div>
       </div>
-    );
+  );
 }
